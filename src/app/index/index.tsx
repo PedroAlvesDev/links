0bf79c6fd1429f7fd47146fdda580b5269fbec54
Seu Nome <139656791+PedroAlvesDev@users.noreply.github.com>
2025-08-27 16:47:40 -0300

    Add Categories

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -3,6 +3,7 @@ import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./styles";
 import { colors } from "@/styles/colors";
+import { categories } from "@/utils/categories";
 
 import { Category } from "@/components/category";
 
@@ -16,10 +17,13 @@ export default function Index() {
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
-            <Category name="Projeto" icon="code" isSelected />
+            <Category
+                name={categories[0].name}
+                icon={categories[0].icon}
+                isSelected
+            />
             <Category name="Site" icon="language" isSelected={false} />
             <Category name="Video" icon="movie" isSelected={false} />
-
         </View>
     )
 };
