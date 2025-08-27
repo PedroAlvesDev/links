97ed71c0fe330a5a84c5df28669536e9e96fbaec
Seu Nome <139656791+PedroAlvesDev@users.noreply.github.com>
2025-08-27 12:09:00 -0300

    Add Components, Props, Styles

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,12 +1,25 @@
-import { Text, View } from "react-native";
-import { styles } from "./styles";
+import { View, Image, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons"
 
+import { styles } from "./styles";
+import { colors } from "@/styles/colors";
+
+import { Category } from "@/components/category";
 
 export default function Index() {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Hello </Text>
-            <Text style={styles.title}>React Native!</Text>
+            <View style={styles.header}>
+                <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+                <TouchableOpacity>
+                    <MaterialIcons name="add" size={32} color={colors.green[300]} />
+                </TouchableOpacity>
+            </View>
+            <Category name="Projeto" icon="code" isSelected />
+            <Category name="Site" icon="language" isSelected={false} />
+            <Category name="Video" icon="movie" isSelected={false} />
+
         </View>
     )
 };
