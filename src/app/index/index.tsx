073a1fb897629dbbcb161d34c55e09c15a33d4dd
Seu Nome <139656791+PedroAlvesDev@users.noreply.github.com>
2025-08-28 12:32:00 -0300

    Navigation States

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons"
 
@@ -6,18 +7,23 @@ import { colors } from "@/styles/colors";
 import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
 import { Option } from "@/components/option";
+import { router } from "expo-router";
+import { categories } from "@/utils/categories";
+
 
 export default function Index() {
+    const [category, setCategory] = useState(categories[0].name)
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => router.navigate("/add")}>
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
-            <Categories />
+            <Categories onChange={setCategory} selected={category} />
 
             <FlatList
                 data={["1", "2", "3", "4", "5"]}
@@ -34,7 +40,7 @@ export default function Index() {
                 showsVerticalScrollIndicator={false}
             />
 
-            <Modal transparent visible={true}>
+            <Modal transparent visible={false}>
                 <View style={styles.modal}>
                     <View style={styles.modalContent}>
                         <View style={styles.modalHeader}>
